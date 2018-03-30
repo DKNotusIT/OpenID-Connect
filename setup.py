44f5a8fb40cda9a1ef576ea5f2e168aac6079860
DKNOTUS\mateusz.szlendak <mateusz.szlendak@notusfinanse.pl>
2018-03-30 12:47:16 +0200

    example rREADME.md

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'oidc_lib',
-    version = '0.0.1',
-    description = 'A simple library to connect with OpenID',
-    url = 'https://gitlab.atm.dknotus.pl:8888/notus/oidclib',
-    author = 'Notus Finanse',
+    name='oidc_lib',
+    version='0.0.1',
+    description='A simple library to connect with OpenID',
+    url='https://gitlab.atm.dknotus.pl:8888/notus/oidclib',
+    author='Notus Finanse S.A',
+    author_email='it.rozwoj@notusfinanse.pl',
     classifiers=[
         'Development Status :: 3 -  Alpha',
         'Intended Audience :: Developers',
@@ -15,12 +16,5 @@ setup(
     packages=find_packages(),
     install_requires=['flask'],
     python_requires='>=3.5.2',
-
-
-
-
-
-
-
 
 )
